c2cc5fede1350350e0c5f5c18ed0666ed713522f
aslpk21 <162812762+aslpk21@users.noreply.github.com>
2024-11-11 03:48:55 +0700

    Add files via upload

---
+++ Lab3/Assignment4/NgayThangNam.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class NgayThangNam {
+private:
+    int iNgay, iThang, iNam;
+
+public:
+    NgayThangNam();
+    NgayThangNam(int Nam, int Thang, int Ngay);
+    int TinhNgay();
+    NgayThangNam operator+(int ngay);
+    NgayThangNam operator-(int ngay);
+    NgayThangNam operator-(NgayThangNam a);
+    bool operator==(NgayThangNam a);
+    bool operator!=(NgayThangNam a);
+    bool operator>=(NgayThangNam a);
+    bool operator<=(NgayThangNam a);
+    bool operator>(NgayThangNam a);
+    bool operator<(NgayThangNam a);
+    NgayThangNam operator++();
+    NgayThangNam operator++(int);
+    NgayThangNam operator--();
+    NgayThangNam operator--(int);
+    friend istream& operator>>(istream& in, NgayThangNam& nt);
+    friend ostream& operator<<(ostream& out, NgayThangNam& nt);
+};
