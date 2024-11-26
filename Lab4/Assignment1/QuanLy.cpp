e076ec54bd4bee07c886df35ba570341c30b9418
aslpk21 <162812762+aslpk21@users.noreply.github.com>
2024-11-26 23:59:41 +0700

    Update QuanLy.cpp

--- Lab4/Assignment1/QuanLy.cpp
+++ Lab4/Assignment1/QuanLy.cpp
@@ -1,20 +1,27 @@
 #include "QuanLy.h"
 
-QuanLy::QuanLy() : NhanVien(){
+QuanLy::QuanLy() : NhanVien() {
     tyLeThuong = 0;
 }
 
-double QuanLy::TienThuong(){
+double QuanLy::TienThuong() {
     return (tyLeThuong / 100) * LuongCoBan;
 }
 
 void QuanLy::Nhap() {
+    cout << "Nhap thong tin quan ly:\n";
     NhanVien::Nhap();
-    cout << "Nhap ty le thuong (0 - 100%): ";
-    cin >> tyLeThuong;
+    do {
+        cout << "Nhap ty le thuong (0 - 100%): ";
+        cin >> tyLeThuong;
+        if (tyLeThuong < 0 || tyLeThuong > 100)
+            cout << "Ty le thuong khong hop le. Nhap lai.\n";
+    } while (tyLeThuong < 0 || tyLeThuong > 100);
+    cout << "\n";
 }
 
-void QuanLy::Xuat(){
+void QuanLy::Xuat() {
+    cout << "\nThong tin quan ly: \n";
     NhanVien::Xuat();
-    cout << "Tien thuong: " << TienThuong() << "\n";
+    cout << "Tien thuong: " << fixed << setprecision(0) << TienThuong() << " VND\n";
 }
