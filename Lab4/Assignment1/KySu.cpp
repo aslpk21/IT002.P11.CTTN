bcc28a3f673fd61aab456edb78dc09c3a12ce340
aslpk21 <162812762+aslpk21@users.noreply.github.com>
2024-11-26 23:59:20 +0700

    Update KySu.cpp

--- Lab4/Assignment1/KySu.cpp
+++ Lab4/Assignment1/KySu.cpp
@@ -1,20 +1,22 @@
 #include "KySu.h"
 
-KySu::KySu() : NhanVien(){
+KySu::KySu() : NhanVien() {
     soGioLamThem = 0;
 }
 
-double KySu::TienThuong(){
+double KySu::TienThuong() {
     return soGioLamThem * 100000;
 }
 
-void KySu::Nhap(){
+void KySu::Nhap() {
+    cout << "Nhap thong tin ky su: \n";
     NhanVien::Nhap();
     cout << "Nhap so gio lam them: ";
     cin >> soGioLamThem;
 }
 
-void KySu::Xuat(){
+void KySu::Xuat() {
+    cout << "\nThong tin ky su: \n";
     NhanVien::Xuat();
-    cout << "Tien thuong: " << TienThuong() << " VND\n";
+    cout << "Tien thuong: " << fixed << setprecision(0) << TienThuong() << " VND\n";
 }
